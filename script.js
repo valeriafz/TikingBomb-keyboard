47b14d6dc53127ded083e0b7732ecd3b5ea3d400
valeriafz <valeria.frunza33@gmail.com>
2023-11-22 15:43:24 +0200

    Add alerts for winning and losing

--- script.js
+++ script.js
@@ -2,64 +2,47 @@ const keys = document.querySelectorAll(".keys");
 const resultEls = document.querySelectorAll(".result div");
 const countDown = document.querySelector(".countdown");
 
+const targetArray = ["A", "B", "C", "5"];
+const keyMap = new Map();
+
+let won = 0;
+let resultIndex = 0;
+let timeLeft = 60;
+
 const resetResult = (e) => {
   e.target.classList.add("reset");
-  e.target.textContent = "0";
+  //   e.target.textContent = "0";
+  //  nu merge normal sa fie resetate la 0
 };
 
-let timeLeft = 60;
+resultEls.forEach((item) => {
+  item.addEventListener("transitionend", resetResult);
+});
 
 const updateTime = () => {
   const minutes = Math.floor(timeLeft / 60);
   let seconds = timeLeft % 60;
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
-
   countDown.innerHTML = `${minutes}:${seconds}`;
 
   timeLeft--;
 
   if (timeLeft < 0) {
     clearInterval(interval);
-    resetResult;
     return;
+  } else if (timeLeft === 0) {
+    timeLeft = 60;
+    alert("You lost! The bomb has exploded!");
   }
 };
 
 const interval = setInterval(updateTime, 1000);
 
-const checkResult = () => {
-  let won = true;
-
-  resultEls.forEach((resultEl) => {
-    if (resultEl.style.backgroundColor === "red") {
-      won = false;
-    }
-  });
-
-  if (won) {
-    alert("You won!");
-    updateTime;
-  } else {
-    alert("You lost!");
-    updateTime;
-  }
-};
-
-const targetArray = ["A", "B", "C", "D"];
-
-const keyMap = new Map();
-
 keys.forEach((key) => {
   keyMap.set(key.innerText, key);
   keyMap.set(key.innerText.toLowerCase(), key);
 });
-
-resultEls.forEach((item) => {
-  item.addEventListener("transitionend", resetResult);
-});
-
-let resultIndex = 0;
 
 const handleKey = (e) => {
   const keyEl = keyMap.get(e.key);
@@ -73,17 +56,24 @@ const handleKey = (e) => {
 
     if (/^[A-Z0-9]$/.test(key)) {
       resultEls[resultIndex].textContent = key;
-
-      resultIndex++;
-
-      if (resultIndex === resultEls.length) {
-        resultEls.forEach((resultEl, index) => {
-          if (resultEl.textContent === targetArray[index]) {
-            resultEl.style.backgroundColor = "green";
+      won = 0;
+      if (resultIndex < resultEls.length - 1) {
+        resultIndex++;
+      } else {
+        resultEls.forEach((el, index) => {
+          if (el.textContent === targetArray[index]) {
+            el.style.backgroundColor = "green";
+            won++;
           } else {
-            resultEl.style.backgroundColor = "red";
+            el.style.backgroundColor = "red";
           }
         });
+
+        if (won === 4) {
+          timeLeft = 60;
+          updateTime();
+          alert("You won! The bomb has been diffused!");
+        }
 
         resultEls.forEach((el) => {
           el.classList.remove("reset");
